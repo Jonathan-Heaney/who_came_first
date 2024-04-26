38468dd88e1558da1355989c41306a1128e74d56
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-26 16:35:45 -0500

    first commit

---
+++ who_came_first/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for who_came_firstt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'who_came_firstt.settings')
+
+application = get_wsgi_application()
