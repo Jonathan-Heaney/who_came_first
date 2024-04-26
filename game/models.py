8d6aea7ab69a09257384e7553271325e65e6c9e3
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-26 16:49:08 -0500

    Make models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class FamousPerson(models.Model):
+    name = models.CharField(max_length=1000)
+    occupation = models.CharField(max_length=255)
+    gender = models.CharField(max_length=2)
+    alive = models.BooleanField(null=True, blank=True)
+    bplace_name = models.CharField(max_length=2000, null=True, blank=True)
+    bplace_country = models.CharField(max_length=255, null=True, blank=True)
+    birthdate = models.CharField(max_length=20, null=True, blank=True)
+    birthyear = models.IntegerField(null=True, blank=True)
+    dplace_name = models.CharField(max_length=2000, null=True, blank=True)
+    dplace_country = models.CharField(max_length=255, null=True, blank=True)
+    deathdate = models.CharField(max_length=20, null=True, blank=True)
+    deathyear = models.IntegerField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    hpi = models.FloatField(db_index=True)
+
+    def __str__(self):
+        return self.name
